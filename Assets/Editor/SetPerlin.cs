c225e8a731715cff517716f1639a5d3fc9604eb0
Jonathan-D-M <72343982+Jonathan-D-M@users.noreply.github.com>
2021-10-11 11:07:58 +0100

    Create SetPerlin.cs

---
+++ Assets/Editor/SetPerlin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PerlinNoise))]
+public class SetPerlin : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PerlinNoise thisscript = (PerlinNoise)target;
+        //thisscript.loopvalx =  EditorGUILayout.IntField("X value", thisscript.loopvalx);
+        //thisscript.loopvaly = EditorGUILayout.IntField("Y value", thisscript.loopvaly);
+
+        if(GUILayout.Button("Run With New Values"))
+        {
+            //redo all this with the new values.
+            //thisscript
+            thisscript.clearSlate();
+
+            if(thisscript.getMultipleLayers())
+            {
+                thisscript.generateMultiLayerGrid(thisscript.getLayerCount());
+            }
+            else
+            {
+                thisscript.generate1LayerGrid();
+            }
+        }
+    }
+
+
+}
